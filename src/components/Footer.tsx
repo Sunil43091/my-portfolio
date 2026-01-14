ad79dca91efd359fcd0df2ae6c27eedcff74be73
Sunil43091 <102575989+Sunil43091@users.noreply.github.com>
2026-01-14 20:53:22 +0530

    fix vercel routing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import {
   Twitter,
   Mail,
   Phone,
-  MapPinHouse 
+  MapPinHouse,
 } from "lucide-react";
 import { Link } from "react-router-dom";
 
@@ -16,25 +16,22 @@ export default function Footer() {
         <div className="grid md:grid-cols-3 gap-10 border-t border-white/10 pt-10">
           {/* BRAND */}
           <div className="md:text-start text-center">
-            <h2 className="text-3xl font-semibold text-(--primary) mb-3">
-                 <Link to="/">
-        <img src="/logo.svg" alt="" className="w-10 h-10"/>
-        </Link>
+            <h2 className="text-3xl font-semibold text-(--primary) mb-5">
+              <Link to="/">
+                <img src="/logo.svg" alt="" className="w-15 h-15 mx-auto md:mx-0" />
+              </Link>
             </h2>
             <p className="text-sm leading-relaxed">
-  Crafting modern, visually appealing websites with clean UI
-  and intuitive user experiences. Focused on smooth animations,
-  responsive layouts, and strong performance to deliver
-  high-quality digital solutions that help brands grow online.
-</p>
-
+              Crafting modern, visually appealing websites with clean UI and
+              intuitive user experiences. Focused on smooth animations,
+              responsive layouts, and strong performance to deliver high-quality
+              digital solutions that help brands grow online.
+            </p>
           </div>
 
           {/* LINKS */}
           <div className="md:text-start text-center">
-            <h3 className="text-(--primary) mb-4 font-medium">
-              Quick Links
-            </h3>
+            <h3 className="text-(--primary) mb-4 font-medium">Quick Links</h3>
             <ul className="space-y-2 text-sm">
               {["Home", "About", "Projects", "Skills", "Contact"].map(
                 (item) => (
@@ -52,20 +49,18 @@ export default function Footer() {
           </div>
           {/* CONTACT */}
           <div className="md:text-start text-center">
-            <h3 className="text-(--primary) mb-4 font-medium">
-              Get in Touch
-            </h3>
+            <h3 className="text-(--primary) mb-4 font-medium">Get in Touch</h3>
 
             <div className="flex items-center gap-3 md:justify-start justify-center text-sm mb-4">
-               <Phone  className="text-(--primary)" size={16} />
+              <Phone className="text-(--primary)" size={16} />
               <span>+919056343091</span>
-            </div> 
+            </div>
             <div className="flex items-center gap-3 md:justify-start justify-center text-sm mb-4">
-               <Mail className="text-(--primary)" size={16} />
+              <Mail className="text-(--primary)" size={16} />
               <span>sk3645797@gmail.com</span>
             </div>
             <div className="flex items-center gap-3 md:justify-start justify-center text-sm mb-4">
-               <MapPinHouse className="text-(--primary)" size={16} />
+              <MapPinHouse className="text-(--primary)" size={16} />
               <span>Patiala Punjab 140701</span>
             </div>
 
@@ -91,22 +86,21 @@ export default function Footer() {
         </div>
 
         {/* BOTTOM */}
-        <div className="
+        <div
+          className="
           mt-14 pt-6 border-t border-white/10
           flex flex-col md:flex-row
           items-center justify-between
           text-xs gap-4
-        ">
-          <p>
-            © {new Date().getFullYear()} SP. All rights reserved.
-          </p>
+        "
+        >
+          <p>© {new Date().getFullYear()} SP. All rights reserved.</p>
 
           <p>
             Designed & Built with ❤️ by{" "}
             <span className="text-(--primary)">Sunil Kumar</span>
           </p>
         </div>
-
       </div>
     </footer>
   );
